dd146e89ee7bc11583143dda339074244a570abc
Daniel Nichols <herrnichols@gmail.com>
2021-08-24 11:12:35 -0400

    updated dependencies

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,8 @@
-require('dotenv').config();
+const { NODE_ENV, DATABASE_URL, TEST_DATABASE_URL } = require('./src/config');
 
 module.exports = {
   migrationsDirectory: 'migrations',
   driver: 'pg',
-  connectionString:
-    process.env.NODE_ENV === 'test'
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+  connectionString: NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
   ssl: !!process.env.SSL,
 };
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'postgresql://daniel@localhost/spaced-repetition',
+    'postgresql://postgres:pass@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'deutschewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
